967f262c4a8dfe1f14360565144d277c72a3c4c7
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-07-27 10:49:27 +0300

    Add eslint configuration with the needed scripts to run the application

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["airbnb-base", "airbnb-typescript/base"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "import/prefer-default-export": [
+      "off" | "warn" | "error",
+      { target: "any" },
+    ],
+  },
+};
