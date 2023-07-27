4b42068cf0759c53936d566b5eb496091c644fe8
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-07-27 10:57:35 +0300

    Setup and configuration for Jasmine unit testing

---
+++ src/tests/helpers/reporter.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  DisplayProcessor,
+  SpecReporter,
+  StacktraceOption,
+  // eslint-disable-next-line @typescript-eslint/quotes
+} from "jasmine-spec-reporter";
+import SuiteInfo = jasmine.SuiteInfo;
+
+class CustomProcessor extends DisplayProcessor {
+  // eslint-disable-next-line class-methods-use-this
+  public displayJasmineStarted(info: SuiteInfo, log: string): string {
+    return `${log}`;
+  }
+}
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(
+  new SpecReporter({
+    spec: {
+      displayStacktrace: StacktraceOption.NONE,
+    },
+    customProcessors: [CustomProcessor],
+  })
+);
