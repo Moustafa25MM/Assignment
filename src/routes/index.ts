882a29e6b8e94aec46f4c750382f3e1a31a2e63d
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-07-30 17:55:19 +0300

    removed swagger docs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,78 +11,10 @@ import { logoutoutes } from '../middlewares/logout';
 
 const router = Router();
 
-/**
- * @swagger
- * /register:
- *   post:
- *     summary: Register a new user
- *     description: Register a new user with their name, email, and password
- *     tags:
- *       - Authentication
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/definitions/User'
- *     responses:
- *       200:
- *         description: User registered successfully
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/definitions/User'
- *       400:
- *         description: Invalid data format or email already exists
- *       500:
- *         description: Internal server error
- */
 router.post('/register', validations.checkEmail, errorHandling(authMethods.registerMiddleware));
 
-/**
- * @swagger
- * /login:
- *   post:
- *     summary: User login
- *     description: Logs in a user with their email and password
- *     tags:
- *       - Authentication
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/definitions/Login'
- *     responses:
- *       200:
- *         description: User logged in successfully
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/definitions/User'
- *       400:
- *         description: Invalid email or password
- *       401:
- *         description: Unauthorized user
- *       500:
- *         description: Internal server error
- */
 router.use('/login', errorHandling(loginMethods.userLogin));
 
-/**
- * @swagger
- * /logout:
- *   post:
- *     summary: User logout
- *     description: Logs out a user by clearing the JWT token cookie
- *     tags:
- *       - Authentication
- *     responses:
- *       200:
- *         description: User logged out successfully
- *       500:
- *         description: Internal server error
- */
 router.use(logoutoutes.logout.path, logoutoutes.logout.middleware, logoutoutes.logout.handler);
 
 router.use('/jogging', authMethods.userAuth, errorHandling(joggingRoutes));
